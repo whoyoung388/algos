c74106f1a6d9f45dd501ab1be3707e21247d07da
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-05-13 23:11:42 -0400

    215 java solution

---
+++ kth-largest-element-in-an-array.js
@@ -0,0 +1,35 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+    
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start == end) {
+            return nums[start];
+        }
+        int i = start, j = end, pivot = nums[(i + j) / 2];
+        while (i <= j) {
+            while (i <= j && nums[i] > pivot) {
+                i++;
+            }
+            while (i <= j && nums[j] < pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        
+        if (start + k - 1 <= j) {
+            return quickSelect(nums, start, j, k);
+        }
+        if (start + k - 1 >= i) {
+            return quickSelect(nums, i, end, k - i + start);
+        }
+        return nums[j + 1];
+    }
+}
