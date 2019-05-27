7f40f33beee04772a4b87cdd1410121848429608
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-05-27 16:46:12 -0400

    155 java solution 2

--- min-stack.java
+++ min-stack.java
@@ -1,3 +1,51 @@
+/* Solution I */
+class Node {
+    int val, minVal;
+    Node next;
+    
+    public Node(int v, int min) {
+        this(v, min, null);
+    }
+    
+    public Node(int v, int min, Node next) {
+        this.val = v;
+        this.minVal = min;
+        this.next = next;
+    }
+}
+
+
+class MinStack {
+
+    Node head;
+    int minVal;
+    
+    public void push(int x) {
+        if (head == null) {
+            minVal = x;
+        } else {
+            minVal = Math.min(minVal, x);
+        }
+        head = new Node(x, minVal, head);
+    }
+    
+    public void pop() {
+        head = head.next;
+        if (head == null) return;
+        minVal = head.minVal;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.minVal;
+    }
+}
+
+
+/* Solution II */
 class MinStack {
 
     Stack<Integer> stack;
