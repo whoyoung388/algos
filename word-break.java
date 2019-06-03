1b92272d2f73fea76ace988736519e3cfbb8c28f
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-06-03 13:27:55 -0400

    139 java solution

--- word-break.java
+++ word-break.java
@@ -19,6 +19,31 @@ class Solution {
     }
 }
 
+// DFS + memoization
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return dfs(s, wordDict, 0, new Boolean[s.length()]);
+    }
+    
+    private boolean dfs(String s, List<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        
+        for (int end = start+1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && dfs(s, wordDict, end, memo)) {
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
+}
+
+
 
 // TLE Naive Solution
 class Solution {
