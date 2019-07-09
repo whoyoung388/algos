ebf0c6707d21f78ebd741ca7d5d6799a3ebcfeb1
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-08 22:14:09 -0400

    200 java solution

--- number-of-islands.java
+++ number-of-islands.java
@@ -1,3 +1,44 @@
+// DFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int islands = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+    
+    private void dfs(char[][] grid, int x, int y) {
+        if (grid[x][y] == '1') {
+            grid[x][y] = '0';
+        }
+        int[] delX = {1, 0, -1, 0};
+        int[] delY = {0, 1, 0, -1};
+        for (int i = 0; i < 4; i++) {
+            int cx = delX[i] + x;
+            int cy = delY[i] + y;
+            if (valid(grid, cx, cy) && grid[cx][cy] == '1') {
+                dfs(grid, cx, cy);
+            }
+        }
+    }
+    
+    private boolean valid(char[][] grid, int x, int y) {
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
+    }
+}
+
+// BFS
 class Coordinate {
     int x, y;
     public Coordinate(int x, int y) {
