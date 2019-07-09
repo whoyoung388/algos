4e028b20b200122846c706ed1104adceb61a509b
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-09 12:16:32 -0400

    207 java solution

--- course-schedule.java
+++ course-schedule.java
@@ -1,3 +1,51 @@
+// DFS
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites.length == 0 || numCourses == 0) {
+            return true;
+        }
+        boolean[] visited = new boolean[numCourses];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<Integer>());
+        }
+        
+        for (int[] pre : prerequisites) {
+            int in = pre[0];
+            int from = pre[1];
+            map.get(from).add(in);
+        }
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, visited, map) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int course, boolean[] visited, HashMap<Integer, List<Integer>> map) {
+        /*
+        Return:
+            true    if no loop found
+            false   if loop exists
+        */
+        if (visited[course]) {
+            return false;
+        }
+        visited[course] = true;
+        List<Integer> nextCourses = map.get(course);
+        for (int c : nextCourses) {
+            if (dfs(c, visited, map) == false) {
+                return false;
+            }
+        }
+        visited[course] = false;
+        return true;
+    }
+}
+
+// BFS (Faster)
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
