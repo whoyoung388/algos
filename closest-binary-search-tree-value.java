6287cbc9a17fa911c0137e9dcfbcd2b67590f257
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-13 20:18:25 -0400

    270 java solution

--- closest-binary-search-tree-value.java
+++ closest-binary-search-tree-value.java
@@ -7,6 +7,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// Iteration
 class Solution {
     public int closestValue(TreeNode root, double target) {
         int res = root.val;
@@ -20,5 +22,18 @@ class Solution {
             root = root.val > target ? root.left : root.right;
         }
         return res;
+    }
+}
+
+
+// Recursion
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        TreeNode next = root.val < target ? root.right : root.left;
+        if (next == null) {
+            return root.val;
+        }
+        int nextClosest = closestValue(next, target);
+        return Math.abs(root.val - target) < Math.abs(nextClosest - target) ? root.val : nextClosest;
     }
 }
