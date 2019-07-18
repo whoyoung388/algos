74be13b44c39398e41accee9a78127c1bc376afd
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-17 22:34:42 -0400

    300 python solution

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,3 +1,21 @@
+// DP O(n^2)
+class Solution:
+    def lengthOfLIS(self, nums):
+        dp = [1] * len(nums)
+        res = 0
+        for i, num in enumerate(nums):
+            for j in range(i):
+                if nums[j] >= nums[i]:
+                    continue
+                dp[i] = max(dp[i], dp[j]+1)
+
+            if dp[i] > res:
+                res = dp[i]
+        return res
+
+
+
+// DFS (TLE)
 class Solution:
     def lengthOfLIS(self, nums):
         if not nums:
