c056b45c31c9f8fa00a660bceadcebf29a9094f8
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-20 17:25:43 -0400

    797 python solution

--- all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target.py
@@ -1,3 +1,23 @@
+// BFS (a little bit faster)
+import collections
+
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]):
+        res = []
+        que = collections.deque([[0], ])
+        while que:
+            base_path = que.popleft()
+            if base_path[-1] == len(graph) - 1:
+                res.append(base_path)
+                continue
+            for neighbor in graph[base_path[-1]]:
+                new_path = base_path + [neighbor]
+                que.append(new_path)
+        return res
+
+
+// DFS
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res = []
