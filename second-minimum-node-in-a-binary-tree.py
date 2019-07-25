d3263c1262981fc7dbf721fa1b2df57c139954fd
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-24 23:15:28 -0400

    671 python solution

--- second-minimum-node-in-a-binary-tree.py
+++ second-minimum-node-in-a-binary-tree.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+// Faster
 class Solution:
     def findSecondMinimumValue(self, root: TreeNode) -> int:
         res = [root.val, float("inf")]
@@ -19,3 +20,25 @@ class Solution:
         
         if res[0] < root.val < res[1]:
             res[1] = root.val
+
+// Priority Queue
+
+import heapq
+class Solution:
+    def findSecondMinimumValue(self, root) -> int:
+        res = []
+        self.dfs(root, res)
+        while res:
+            val = heapq.heappop(res)
+            if val != root.val:
+                return val
+        return -1
+    
+    def dfs(self, root, res):
+        if not root:
+            return
+        if not root.left:
+            heapq.heappush(res, root.val)
+            return
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
