720e92bd23afa9d60e0452f0608eb363d3ce90ee
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-26 21:55:23 -0400

    238 python solution

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,3 +1,19 @@
+// Space: O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+
+        temp = 1
+        for i in range(len(nums)-2, -1, -1):
+            temp *= nums[i+1]
+            res[i] *= temp
+
+        return res
+
+
+// Space: O(n)
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         rightpass = [1] + nums[:-1]
