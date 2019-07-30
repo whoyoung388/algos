044c79a4506eed7abb69cfbe4d0fa2214db8ec7e
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-07-29 22:48:02 -0400

    647 python solution

--- palindromic-substrings.py
+++ palindromic-substrings.py
@@ -1,3 +1,34 @@
+# Manacher O(n)
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dummy = '#'
+        newS = dummy
+        for char in s:
+            newS += char + dummy
+
+        C, R = 0, 0
+        lps = [0] * len(newS)
+        count = 0
+        for i in range(1, len(newS)):
+            if i < R:
+                mirror_i = 2*C - i
+                lps[i] = min(R - i, lps[mirror_i])
+            lps[i] = self.expand(newS, i, lps[i])
+            count += (lps[i] + 1) // 2
+            if i + lps[i] > R:
+                C = i
+                R = i + lps[i]
+        return count
+    
+    def expand(self, s, i, dis):
+        attemp = dis + 1
+        while i-attemp >= 0 and i+attemp < len(s) and s[i-attemp] == s[i+attemp]:
+            dis = attemp
+            attemp += 1
+        return dis
+
+
+# DP O(n^2)
 class Solution:
     def countSubstrings(self, s: str) -> int:
         dp = [[0] * len(s) for _ in range(len(s))]
