30f77e822b334391430cfb43a76326dab1e43c9f
Yang (Young) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-08-21 10:40:17 -0400

    42 python solution

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -1,3 +1,29 @@
+# two-pointer solution: Time O(n), Space O(1)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        volumn = 0
+        
+        left, right = 0, len(height)-1
+        leftMax, rightMax = 0, 0
+        
+        while left < right:
+            if height[left] <= height[right]:
+                if height[left] > leftMax:
+                    leftMax = height[left]
+                else:
+                    volumn += leftMax - height[left]
+                left += 1
+            else:
+                if height[right] > rightMax:
+                    rightMax = height[right]
+                else:
+                    volumn += rightMax - height[right]
+                right -= 1
+        
+        return volumn
+
+
+# 3-pass solution: Time O(3n), Space O(n)
 class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
