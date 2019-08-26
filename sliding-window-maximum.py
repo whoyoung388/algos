42c0fff2d0e70a94cd4e78835ac6a0aabe818a32
Yang (Yann) Hu <35487405+whoyoung388@users.noreply.github.com>
2019-08-26 09:20:27 -0400

    239 python solution

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -1,3 +1,29 @@
+# Monotonic Queue, Time: O(2N) = O(N)
+from collections import deque
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        res = []
+        monoque = deque()
+        
+        for i in range(len(nums)):
+            while monoque and nums[i] > nums[monoque[-1]]:
+                monoque.pop()
+            monoque.append(i)
+            if i >= k - 1:
+                res.append(nums[monoque[0]])
+            if monoque[0] == i - k + 1:
+                monoque.popleft()
+        
+        return res
+
+
+# Brute Force, Time: O((N-k+1)*k)
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
         """
